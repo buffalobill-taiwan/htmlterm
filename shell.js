a2fec0b01f470d3193a50fd5c32bdaf9f25f1d3f
buffalobill-taiwan <buffalobill-taiwan@users.noreply.github.com>
2026-06-18 09:56:03 +0800

    colorful cowsay

--- shell.js
+++ shell.js
@@ -48,11 +48,15 @@ class DemoShell {
             cowsay: (args) => {
                 const text = args.join(' ') || 'Moo!';
                 const len = text.length;
-                const border = '_'.repeat(len + 2);
-                const top = ' ' + border;
-                const mid = '< ' + text + ' >';
-                const bot = ' ' + '_'.repeat(len + 2);
-                const cow = '        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||\n';
+                const border = '\x1B[33m' + '='.repeat(len + 2) + '\x1B[0m';
+                const top = '  ' + border;
+                const mid = '\x1B[33m< \x1B[1;37m' + text + '\x1B[0m \x1B[33m>\x1B[0m';
+                const bot = '  ' + border;
+                const cow = '\x1B[32m        \\   \x1B[1;37m^__^\x1B[0m\x1B[32m\n' +
+                           '         \\  (\x1B[1;37moo\x1B[0m\x1B[32m)\\_______\n' +
+                           '            (__)\\       )\\/\\\n' +
+                           '                ||----\x1B[33mw\x1B[0m\x1B[32m |\n' +
+                           '                ||     ||\x1B[0m\n';
                 this.print(top + '\n' + mid + '\n' + bot + '\n' + cow);
             },
             ascii: () => {
